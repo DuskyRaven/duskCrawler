9b2c5225fff823b1bc84c64e52d26b21067d056d
DuskyRaven <nkrucinskas@gmail.com>
2023-12-19 22:03:21 +0200

    started testing with user input about saving data to their PC. Started work on a dataset when user wants data on his computer. Also added a bit of a touch up to formatting of data in console

--- duskcrawler/POC_crawler.py
+++ duskcrawler/POC_crawler.py
@@ -1,13 +1,11 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from queue import Queue
 from lxml.etree import HTML
 from requests import get
 import textwrap
-# Global variables
-visited_pages = set()
-visited_articles = set()
-pages_queue = Queue()
-articles_queue = Queue()
+import csv
+# pd data set
+dataset = []
+
+# Crawling
 def process_page(page_url: str):
     print(f"Start page: {page_url}")
     # global visited_pages, visited_articles, pages_queue, articles_queue
@@ -18,7 +16,8 @@ def process_page(page_url: str):
             "//a[contains(@class, 'horo-card border-rad-4 px-1 pt-15 pb-2 text-center')]/@href"
         ):
             full_page_url = page_link
-            print(full_page_url)
+            print("")
+            print(f"Article link: {full_page_url}")
             response1 = get(page_link)
             tree1 = HTML(response1.text)
             for date in tree1.xpath(
@@ -44,43 +43,49 @@ def process_page(page_url: str):
                 if tt1 == tt2:
                     par1 = tree1.xpath("//div[contains(@id, 'content-food')]/text()")
                     par1 = par1.__str__().strip('["\\n ').strip(' \\n"]').strip("['\\n ").strip(" \\n']").strip()
-                    print(title1)
+                    print("\033[1m" + title1 + "\033[0m")
                     print(textwrap.fill(par1))
                 elif tt1 == tt3:
                     par2 = tree1.xpath("//div[contains(@id, 'content-home')]/text()")
                     par2 = par2.__str__().strip('["\\n ').strip(' \\n"]').strip("['\\n ").strip(" \\n']").strip()
-                    print(title1)
+                    print("\033[1m" + title1 + "\033[0m")
                     print(textwrap.fill(par2))
                 elif tt1 == tt4:
                     par3 = tree1.xpath("//div[contains(@id, 'content-dog')]/text()")
                     par3 = par3.__str__().strip('["\\n ').strip(' \\n"]').strip("['\\n ").strip(" \\n']").strip()
-                    print(title1)
+                    print("\033[1m" + title1 + "\033[0m")
                     print(textwrap.fill(par3))
                 elif tt1 == tt5:
                     par4 = tree1.xpath("//div[contains(@id, 'content-teen')]/text()")
                     par4 = par4.__str__().strip('["\\n ').strip(' \\n"]').strip("['\\n ").strip(" \\n']").strip()
-                    print(title1)
+                    print("\033[1m" + title1 + "\033[0m")
                     print(textwrap.fill(par4))
                 elif tt1 == tt6:
                     par5 = tree1.xpath("//div[contains(@id, 'content-cat')]/text()")
                     par5 = par5.__str__().strip('["\\n ').strip(' \\n"]').strip("['\\n ").strip(" \\n']").strip()
-                    print(title1)
+                    print("\033[1m" + title1 + "\033[0m")
                     print(textwrap.fill(par5))
                 elif tt1 == tt7:
                     par6 = tree1.xpath("//div[contains(@id, 'content-bonus')]/text()")
                     par6 = par6.__str__().strip('["\\n ').strip(' \\n"]').strip("['\\n ").strip(" \\n']").strip()
-                    print(title1)
+                    print("\033[1m" + title1 + "\033[0m")
                     print(textwrap.fill(par6))
                 else:
                     print("error")
-
-
-
-
-            if full_page_url not in visited_pages:
-                pages_queue.put(full_page_url)
-                visited_pages.add(full_page_url)
     except Exception as e:
         print("Error retrieving url:", e)
+
 print("Example data: ")
 process_page("https://www.astrology.com/horoscope/daily.html")
+# Testing inputs from user
+while True:
+    prompt1 = input("Would you like for the data to be saved on your computer?").lower()
+    if prompt1 == "yes":
+        print("Hooray")
+        break
+    elif prompt1 == "no":
+        print("T_T")
+        break
+    else:
+        print("whaa?")
+        print("Answer must be a 'yes' or a 'no' ")
